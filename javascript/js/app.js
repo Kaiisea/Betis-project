2589a35a232e74b2ac3988896c59d45726cecbfa
kai <abacatemarinheiro@gmail.com>
2022-03-18 16:18:34 +0100

    gridy avanced

---
+++ javascript/js/app.js
@@ -0,0 +1,4 @@
+let msg = "Hello, " + "world";
+let userName = "John";
+let greeting = "Welcome," + " " + userName;
+let literal = `Welcome, ${userName}`;
--- js/app.js
+++
@@ -1,2 +0,0 @@
-//investigar como cargar primero todo el HTML
-//interactuar con un boton para los tema claro y oscuro
